d9266141e8a7c12fd4c2eeaded36aa3b7e39561f
Ali-Traboulsi <alitraboulsi112.gmail.com>
2020-10-21 13:58:52 +0300

    Step-4

--- index.js
+++ index.js
@@ -31,6 +31,43 @@ app.get('/time', (req, res) => {
     });
 });
 
+app.get('/hello', (req, res) => {
+    res.send({
+        status: 200,
+        message: "Hello" 
+    });
+});
+
+app.get('/hello/:path*', (req, res) => {
+    res.send({
+        status: 200,
+        message: "Hello, " + req.params.path
+    });
+});
+
+app.get('/search', (req, res) => {
+    const search = req.query.s;
+
+    if (typeof search != 'undefined'){
+        const response = {
+            status: 200,
+            message: "ok",
+            data: search
+        };
+        res.send(response);
+    }
+    else {
+        const response = {
+            status: 500,
+            error: true,
+            message: "You have to provide a search"
+        };
+        res.status(500);
+        res.send(response);
+    }
+});
+
+
  app.use((req, res) => {
      res.type('text/plain');
      res.status(404);
